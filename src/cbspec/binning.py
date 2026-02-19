c9d597ae0f1c7188f0d7de729046eeca68f708a5
Rob <r_davignon@hrlx33.cosmic.utah.edu>
2026-02-19 15:20:21 -0700

    reworked binning.py to ONLY do energy binning using numpy.histogram instead of matplotlib hitograms.

--- src/cbspec/binning.py
+++ src/cbspec/binning.py
@@ -1,27 +1,42 @@
-import matplotlib.pyplot as plt
-import config
+"""
+Energy binning utilities in log10(E/eV) space.
+"""
+
+import numpy as np
 
 
-def en_bins(infile):
-    herec = plt.hist(infile[0], bins=config.ENERGYRANGE)
-    hedt = plt.hist(infile[1], bins=config.ENERGYRANGE)
-    hethr = plt.hist(infile[2], bins=config.ENERGYRANGE)
+def make_energy_bins(en_range):
+    """
+    Constructs bin edges, centers, and widths from log10(E/eV) edges
+    :param en_range:
+    :return:
+    """
+    edges = np.asarray(en_range, dtype=float)
+    centers = 0.5 * (edges[1:] + edges[:-1])
+    widths = edges[1:] - edges[:-1]
+    return edges, centers, widths
 
-    ratio_logens = []
-    N_logens = []
-    nr = []
-    nt = []
-    log10en = []
-    for i in range(len(herec[0])):
-        if hethr[0][i] > 1 and hethr[1][i] > config.COENERGY:
-            r = herec[0][i]
-            nr.append(r)
-            t = hethr[0][i]
-            nt.append(t)
-            d = r / t
-            n = hedt[0][i]
-            ratio_logens.append(d)
-            N_logens.append(n)
-            log10en.append(hedt[1][i])
 
-    return log10en, N_logens, ratio_logens
+def histogram_events(log_energy, edges):
+    """
+    Histogram events into energy bins.
+    :param log_energy:
+    :param edges:
+    :return:
+    """
+    counts, _ = np.histogram(log_energy, bins=edges)
+    return counts
+
+
+def histgram_data_per_bin(mc_log_energy, dt_log_energy, mc_raw_log_energy, edges):
+    """
+    Histogram MC & data into the same energy bins.
+    :param mc_log_energy:
+    :param dt_log_energy:
+    :param edges:
+    :return:
+    """
+    mc_counts = histogram_events(mc_log_energy, edges)
+    dt_counts = histogram_events(dt_log_energy, edges)
+    mc_raw_counts = histogram_events(mc_raw_log_energy, edges)
+    return mc_counts, dt_counts, mc_raw_counts
